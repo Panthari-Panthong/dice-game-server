14f81877d606076b3d7a939e10f12368fc865a40
P Panthong <panthari.panthong@gmail.com>
2019-10-28 14:42:10 +0100

    set user unique

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const databaseUrl = 'postgres://postgres:dice@localhost:5432/postgres'
 const db = new Sequelize(databaseUrl)
 
-db.sync()
+db.sync({ force: true })
   .then(() => console.log("Database connected"))
   .catch(console.error)
 
