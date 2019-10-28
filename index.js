fb33d6a33b2ae526e0c0726cd91ea2d0d1335ab1
P Panthong <panthari.panthong@gmail.com>
2019-10-28 14:45:33 +0100

    setup heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 const db = require('./db')
 
 
